cb64f22d23d5da207b50e34005c27fa0bf81bd79
Darrin Eden <darrin.eden@gmail.com>
2009-12-18 17:52:37 -0800

    correct license

--- jruby/metadata.rb
+++ jruby/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Heavy Water Software, Inc."
 maintainer_email  "darrin@heavywater.ca"
-license           "GNUv3"
+license           "MIT"
 description       "Installs jruby"
 
 %w{ java ant git }.each do |d|
