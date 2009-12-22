780e76d323a67139eb21a50645e27ad93d517da5
Darrin Eden <darrin.eden@gmail.com>
2009-12-22 08:47:03 -0800

    moved glassfish+jdbc+postgres into glassfish cookbook

--- jdbc-glassfish-postgresql/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer        "Heavy Water Software Inc."
-maintainer_email  "darrin@heavywater.ca"
-license           "MIT"
-description       "Installs Postgres JDBC library into GlassFish"
-
-supports 'ubuntu'
