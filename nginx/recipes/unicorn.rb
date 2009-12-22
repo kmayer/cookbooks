c4afdc5cc095002c96ad8cd9e5ec3d5f3e4c4884
Darrin Eden <darrin.eden@gmail.com>
2009-12-22 09:17:52 -0800

    moved bits into nginx::unicorn recipe

--- nginx/recipes/unicorn.rb
+++ nginx/recipes/unicorn.rb
@@ -1,2 +1,14 @@
 include_recipe "nginx"
 
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
+end
+
+link "/etc/nginx/sites-enabled/app" do
+  to "/etc/nginx/sites-available/app"
+end
+
+template "/etc/nginx/sites-available/app" do
+  source "app.erb"
+  notifies :restart, resources(:service => "nginx")
+end
