85e9ae32260c9f2e3d4d09cbbbbe9322e41eda71
Darrin Eden <darrin.eden@gmail.com>
2010-01-11 13:32:05 -0800

    use notify for memcached

--- memcached/recipes/default.rb
+++ memcached/recipes/default.rb
@@ -2,11 +2,17 @@ package "memcached" do
   action :upgrade
 end
 
+service "memcached" do
+  supports :status => true, :restart => true
+  action :enable
+end
+
 remote_file "/etc/default/memcached" do
   source "memcached"
   owner "root"
   group "root"
   mode "644"
+  notifies :restart, resources(:service => "memcached")
 end
 
 template "/etc/memcached.conf" do
@@ -15,9 +21,5 @@ template "/etc/memcached.conf" do
   owner "root"
   group "root"
   variables( :memory_mb => node[:memcached][:memory_mb].to_i)
-end
-
-service "memcached" do
-  supports :status => true, :restart => true
-  action [ :enable, :start ]
+  notifies :restart, resources(:service => "memcached")
 end
