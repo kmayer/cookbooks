0fe03f3207ae8e15da31e4530acdc968dbba6d4b
Darrin Eden <darrin.eden@gmail.com>
2010-01-11 12:19:56 -0800

    enable memcached

--- memcached/recipes/default.rb
+++ memcached/recipes/default.rb
@@ -2,10 +2,22 @@ package "memcached" do
   action :upgrade
 end
 
+remote_file "/etc/default/memcached" do
+  source "memcached"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
 template "/etc/memcached.conf" do
   source "memcached.conf.erb"
   mode "644"
   owner "root"
   group "root"
   variables( :memory_mb => node[:memcached][:memory_mb].to_i)
+end
+
+service "memcached" do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
 end
