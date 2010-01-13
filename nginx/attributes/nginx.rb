1575291d914d83eac14b820755d55665c5adae2d
Darrin Eden <darrin.eden@gmail.com>
2010-01-12 17:19:08 -0800

    loosened ssl requirements, fix name collision

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -1,6 +1,6 @@
 set_unless[:nginx][:version] = "0.7.64"
 set_unless[:nginx][:ssl] = "false"
-set_unless[:nginx][:log_format_name] = "combined"
+set_unless[:nginx][:log_format_name] = "default"
 set_unless[:nginx][:log_format] = %q('$remote_addr - $remote_user [$time_local]  '
                     '"$request" $status $body_bytes_sent '
                     '"$http_referer" "$http_user_agent"')
