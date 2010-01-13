c2b25d6becf80c392e6a7438430d76bd69d05a25
Darrin Eden <darrin.eden@gmail.com>
2010-01-12 17:19:23 -0800

    redmine cookbook

---
+++ redmine/recipes/default.rb
@@ -0,0 +1,39 @@
+include_recipe "www-data"
+include_recipe "ruby-enterprise-edition"
+include_recipe "rake"
+include_recipe "unicorn"
+include_recipe "postfix"
+include_recipe "nginx::unicorn"
+include_recipe "postgresql::server"
+include_recipe "postgresql::create"
+include_recipe "postgresql::ruby-pg"
+
+remote_file "/var/www/releases/redmine-0.9.0.tar.gz" do
+  source "http://rubyforge.org/frs/download.php/68515/redmine-0.9.0.tar.gz"
+  checksum "1117365d"
+end
+
+execute "untar redmine" do
+  command "tar xzf redmine-0.9.0.tar.gz"
+  creates "/var/www/releases/redmine-0.9.0/README.rdoc"
+  cwd "/var/www/releases"
+  user "www-data"
+  group "www-data"
+  action :run
+end
+
+link "/var/www/current" do
+  to "/var/www/releases/redmine-0.9.0"
+end
+
+template "/var/www/current/config/database.yml" do
+  source "database.yml.erb"
+  owner "www-data"
+  group "www-data"
+  mode "644"
+  variables( 
+    :db_name => node[:postgresql][:db_name],
+    :username => node[:postgresql][:username],
+    :password => node[:postgresql][:password])
+  notifies :restart, resources(:service => "unicorn")
+end
