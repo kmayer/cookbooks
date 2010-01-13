710cf124b01bd9351c758f04c313f5f113a50202
Darrin Eden <darrin.eden@gmail.com>
2010-01-13 14:00:30 -0800

    guh. non-interactive hyperic server install is a pita. done wasting time on it for now.

--- hyperic/attributes/hyperic.rb
+++ hyperic/attributes/hyperic.rb
@@ -10,8 +10,21 @@ set_unless[:hyperic][:agent_checksum] = "9b55c58a"
 
 set_unless[:hyperic][:server_uri] = "http://downloads.sourceforge.net/project/hyperic-hq/Hyperic%20HQ%204.2/HQ%204.2.0/hyperic-hq-installer-4.2.0-1260-noJRE.tgz"
 set_unless[:hyperic][:server_checksum] = "ae665549"
+set_unless[:hyperic][:server_install_path] = "/home/hyperic/hyperic-hq-installer"
 
-# this collides across cookbooks. need to discover the correct way to create a database
-# set[:postgresql][:db_name] = 'HQ'
-# set[:postgresql][:username] = 'admin'
-# set[:postgresql][:password] = 'hqadmin'
+=begin
+
+set_unless[:postgresql][:db_name] = 'HQ'
+set_unless[:postgresql][:username] = 'admin'
+set_unless[:postgresql][:password] = 'hqadmin'
+
+{ 
+  "recipes": [  "hyperic::server" ],
+  "postgresql": { 
+    "db_name":  "HQ", 
+    "username": "admin", 
+    "password": "hqadmin" 
+  }
+}
+
+=end
--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -1,3 +1,8 @@
 package "openjdk-6-jdk" do
   action :upgrade
 end
+
+execute "set JAVA_HOME" do
+  command "echo JAVA_HOME=/usr >> /etc/environment"
+  not_if "grep JAVA_HOME /etc/environment"
+end
