191114b4dc24d885bc738896c1cd648e2f9f364b
Darrin Eden <darrin.eden@gmail.com>
2010-01-20 15:47:40 -0800

    link to mongrel_rails

--- mongrel/recipes/default.rb
+++ mongrel/recipes/default.rb
@@ -3,3 +3,13 @@
     action :upgrade
   end
 end
+
+link "/usr/bin/mongrel_rails" do
+  to "/usr/local/bin/mongrel_rails"
+  only_if "test -f /usr/local/bin/mongrel_rails"
+end
+
+link "/usr/bin/mongrel_rails" do
+  to "/var/lib/gems/1.8/bin/mongrel_rails"
+  only_if "test -f  /var/lib/gems/1.8/bin/mongrel_rails"
+end
