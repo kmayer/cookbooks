88ab38c73a50fbc365e47fccb5e90ef33f395c05
Darrin Eden <darrin.eden@gmail.com>
2010-01-21 08:27:14 -0800

    keep the .deb around

--- ruby-enterprise-edition/recipes/default.rb
+++ ruby-enterprise-edition/recipes/default.rb
@@ -1,9 +1,9 @@
-remote_file "/tmp/#{node[:ree][:deb]}" do
+remote_file "/usr/src/#{node[:ree][:deb]}" do
   source node[:ree][:project_url]
   checksum node[:ree][:checksum]
 end
 
 execute "ruby-enterprise" do
-  command "dpkg -i /tmp/#{node[:ree][:deb]}"
+  command "dpkg -i /usr/src/#{node[:ree][:deb]}"
   not_if do `ruby --version`.include? "Ruby Enterprise Edition" end
 end
