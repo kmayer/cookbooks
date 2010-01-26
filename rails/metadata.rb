33bc4d89e64ffdc7c12aef056e0903b9548da0e6
Darrin Eden <darrin.eden@gmail.com>
2010-01-26 14:33:36 -0800

    rails cookbook

---
+++ rails/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Heavy Water Software Inc."
+maintainer_email  "darrin@heavywater.ca"
+license           "MIT"
+description       "Installs Ruby on Rails"
+
+supports 'ubuntu'
---
+++ rails/recipes/default.rb
@@ -0,0 +1,3 @@
+package "rails" do
+  action :upgrade
+end
