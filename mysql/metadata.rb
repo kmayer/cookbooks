35f18d3258657a709cb199ffef9310e5b043d994
Darrin Eden <darrin.eden@gmail.com>
2010-01-27 15:34:39 -0800

    initial pass at a mysql cookbook

---
+++ mysql/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Heavy Water Software Inc."
+maintainer_email  "darrin@heavywater.ca"
+license           "MIT"
+description       "Builds and installs an optimized MySQL"
+
+supports 'ubuntu'
