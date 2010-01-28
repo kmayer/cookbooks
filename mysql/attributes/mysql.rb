175ef66ddde8ff7d88b8817df57a343fbe3bc04e
Darrin Eden <darrin.eden@gmail.com>
2010-01-28 13:21:19 -0800

    removed mysql cookbook

--- mysql/attributes/mysql.rb
+++
@@ -1,2 +0,0 @@
-set_unless[:mysql][:version] = "5.1.42-xtradb-9"
-set_unless[:mysql][:source] = "http://www.percona.com/mysql/xtradb/5.1.42-9/source/mysql-5.1.42-xtradb-9.tar.gz"
--- mysql/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer        "Heavy Water Software Inc."
-maintainer_email  "darrin@heavywater.ca"
-license           "MIT"
-description       "Builds and installs an optimized MySQL"
-
-supports 'ubuntu'
