71962861f10f4bafb4bee31ec22be518a2e73a5f
Darrin Eden <darrin.eden@gmail.com>
2010-02-03 11:38:12 -0800

    updated ree to current release

--- ruby-enterprise-edition/attributes/ruby-enterprise-edition.rb
+++ ruby-enterprise-edition/attributes/ruby-enterprise-edition.rb
@@ -1,11 +1,11 @@
 if kernel[:machine] == "x86_64"
-  deb = "ruby-enterprise_1.8.7-2009.10_amd64.deb"
-  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/66163/#{deb}"
-  set_unless[:ree][:checksum] = "c6d1489b"
+  deb = "ruby-enterprise_1.8.7-2010.01_amd64.deb"
+  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/68720/#{deb}"
+  set_unless[:ree][:checksum] = "2e682eb9"
 else
-  deb = "ruby-enterprise_1.8.7-2009.10_i386.deb"
-  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/66164/#{deb}"
-  set_unless[:ree][:checksum] = "f87da507"
+  deb = "ruby-enterprise_1.8.7-2010.01_i386.deb"
+  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/68718/#{deb}"
+  set_unless[:ree][:checksum] = "0465bd70"
 end
 
 set_unless[:ree][:deb] = deb
