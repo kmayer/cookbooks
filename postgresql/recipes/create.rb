f6c4b842ac3a92c6fa6e4585dcce4ba220b881dd
Darrin Eden <darrin.eden@gmail.com>
2010-02-04 11:06:12 -0800

    avoid psql interactively asking www-data for a password. ugly mixing of concerns. revisit

--- postgresql/recipes/create.rb
+++ postgresql/recipes/create.rb
@@ -2,7 +2,6 @@ db_name = node[:postgresql][:db_name]
 username = node[:postgresql][:username]
 password = node[:postgresql][:password]
 
-
 execute "create #{username} database user" do
   command "createuser --no-createdb --no-createrole --no-superuser #{username} && \
     psql --command \"alter role #{username} with encrypted password '#{password}'\""
@@ -22,4 +21,15 @@ execute "create #{db_name} database" do
       select datname from pg_database where \
       datname = '#{db_name}'"`.strip == "#{db_name}"
   end  
+end
+
+# mixing of concerns to support a downstream process.
+# not happy, but don't have a better idea... yet.
+include_recipe "www-data"
+template "/var/www/.pgpass" do
+  source "pgpass.erb"
+  mode "600"
+  owner "www-data"
+  group "www-data"
+  variables( :username => username, :password => password)
 end
