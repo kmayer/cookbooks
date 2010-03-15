eebd20f54fa5112b646d82440a1cc3f721987592
Darrin Eden <darrin.eden@gmail.com>
2010-03-15 09:35:13 -0700

    pg_hba template

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -3,3 +3,16 @@ include_recipe "postgresql::client"
 package "postgresql" do
   action :upgrade
 end
+
+service "postgresql-8.4" do
+  supports :restart => true, :status => true, :reload => true
+  action :enable
+end
+
+template "/etc/postgresql/8.4/main/pg_hba.conf" do
+  source "pg_hba.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode "640"
+  notifies :reload, resources(:service => "postgresql-8.4")
+end
