0b4d15c7f177c4746450186f7560be171327d6c8
Kenneth Mayer <kmayer@bitwrangler.com>
2010-03-24 14:29:37 -1000

    Postgres Server that allows network connections from with 10.0.0.0/8

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -9,10 +9,22 @@ service "postgresql-8.4" do
   action :enable
 end
 
+template "/etc/postgresql/8.4/main/postgresql.conf" do
+  source "postgresql.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode "640"
+  notifies :reload, resources(:service => "postgresql-8.4")
+end
+
 template "/etc/postgresql/8.4/main/pg_hba.conf" do
   source "pg_hba.conf.erb"
   owner "postgres"
   group "postgres"
   mode "640"
   notifies :reload, resources(:service => "postgresql-8.4")
+  variables( 
+    :username => node[:postgresql][:username]
+    )
 end
+
